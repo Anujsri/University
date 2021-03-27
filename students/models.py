3e1d0501dd8e995f74dddee65be3bd24a1b676c7
Anujsri <anuj96sri@gmail.com>
2021-03-27 18:28:50 +0530

    Added new fields in Student and Instructor Tables

--- students/models.py
+++ students/models.py
@@ -6,6 +6,11 @@ from django.db import models
 class Student(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=256)
+    address = models.TextField(null=True,blank=True)
+    roll_number = models.TextField(unique=True,default='')
+    email = models.TextField(null=True)
+    mobile = models.TextField(null=True)
+    created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
